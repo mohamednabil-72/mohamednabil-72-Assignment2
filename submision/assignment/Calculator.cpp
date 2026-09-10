7cf634a11288133e83b83805d7734ccc4612a0cc
Mohamed Nabil <mohamed.nabil2592005@gmail.com>
2026-09-11 01:09:35 +0300

    feat: implement basic calculator

---
+++ submision/assignment/Calculator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch (operation) {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+
+        case '/':
+            cout << "Result: " << num1 / num2 << endl;
+            break;
+
+        default:
+            cout << "Invalid operation." << endl;
+    }
+
+    return 0;
+}
